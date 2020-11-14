8e6957ac517a2d8f9f77082a37136588cb3a8fee
crq <2631315464@qq.com>
2020-11-14 17:28:58 +0800

    配置logback日志与多环境profile

--- soft-news/soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news/soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -1,16 +1,21 @@
 package com.soft1851.user.controller;
 
 import com.soft.api.controller.user.HelloControllerApi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author crq
  */
 @RestController
+@Slf4j
 public class HelloController implements HelloControllerApi {
 
    @Override
     public  Object hello(){
+       log.info("info:hello");
+       log.info("warn:hello");
+       log.info("error:hello");
        return "hello";
    }
 }
