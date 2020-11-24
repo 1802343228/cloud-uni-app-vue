f3df76cb9aa0fd5f79467f98394566e222afee82
crq <2631315464@qq.com>
2020-11-24 11:42:05 +0800

    文章服务模块准备

--- soft-news/soft-news-model/src/main/java/com/soft1851/pojo/bo/SaveFriendLinkBO.java
+++ soft-news/soft-news-model/src/main/java/com/soft1851/pojo/bo/SaveFriendLinkBO.java
@@ -1,9 +1,13 @@
 package com.soft1851.pojo.bo;
 
+import com.soft1851.validate.CheckUrl;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author crq
@@ -14,7 +18,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SaveFriendLinkBO {
     private String id;
+    @NotNull(message = "请选择保留或删除")
     private Integer isDelete;
+    @NotBlank(message = "友情链接不能为空")
     private String linkName;
+    @NotBlank(message = "友情链接地址不能为空")
+    @CheckUrl
     private String linkUrl;
 }
